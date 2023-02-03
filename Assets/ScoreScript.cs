e87bf04937977097885e3e9e7180a59054139a27
Justin Grubbs <grubbsjr@hendrix.edu>
2023-02-02 21:02:46 -0600

    Edited gameover script

--- Assets/ScoreScript.cs
+++ Assets/ScoreScript.cs
@@ -9,6 +9,7 @@ public class ScoreScript : MonoBehaviour
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI boundaryText;
     private int Scorenum;
+    private bool sentinel = false;
 
 
     // took inspiration from you :)
@@ -23,14 +24,16 @@ public class ScoreScript : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D rectangles)
     {
-        if(rectangles.tag == "Rectangles")
-        {
-            Scorenum++;
-            scoreText.text = "Score: " + Scorenum;
-        } else if (rectangles.tag == "boundary") {
+        if (!sentinel) {
+            if (rectangles.tag == "Rectangles") {
+                Scorenum++;
+                scoreText.text = "Score: " + Scorenum;
+            } else if (rectangles.tag == "boundary") {
                 scoreText.text = "";
                 boundaryText.text = "Game over! Final Score: " + Scorenum;
+                sentinel = true;
             }
+        }
     }
 
 
